57cc47c6a48e5cb5b03727f1528e81624eb55f9d
RomanGalyak <ramagalak4@gmail.com>
2024-11-14 13:02:33 +0300

    Add files via upload

---
+++ 2---1---3.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+
+	int period = 2024 - 1626;
+	double cash = 20;
+
+	printf("Начальная сумма вклада: $%.2lf\n", cash);
+	printf("Временной период: %d лет\n\n", period);
+
+	for (int i = period; i != 0; i--) {
+		cash += cash / 100 * 4;
+	}
+
+	printf("Конечная сумма: $%.2lf\n", cash);
+
+	return 0;
+}
